4738c9913045fa65634c3ce67c9b1aeda1851a3e
Yamato Kataoka <34284486+yamatokataoka@users.noreply.github.com>
2021-10-27 10:12:55 +0000

    use img tag instead of next/image
    The next/image component's default loader is not supported
    when using next export.

--- components/avatar/index.tsx
+++ components/avatar/index.tsx
@@ -1,10 +1,9 @@
-import Image from "next/image";
 import styles from "./avatar.module.scss";
 
 export default function Avatar({ src, alt }) {
   return (
     <div className={styles.avatar}>
-      <Image src={src} alt={alt} width={40} height={40} />
+      <img src={src} alt={alt} width="40" height="40" />
     </div>
   );
 }
