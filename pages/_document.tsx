a209abe9a2c52cf74242b25a3b647d04bf351d7a
Yamato Kataoka <34284486+yamatokataoka@users.noreply.github.com>
2021-10-27 06:41:08 +0000

    fix typo

--- pages/_document.tsx
+++ pages/_document.tsx
@@ -6,7 +6,7 @@ export default class MyDocument extends Document {
       <Html lang="en">
         <Head>
           <link
-            href="https://fonts.googleapis.com/css2?family==Roboto&display=swap"
+            href="https://fonts.googleapis.com/css2?family=Roboto&display=swap"
             rel="stylesheet"
           />
         </Head>
