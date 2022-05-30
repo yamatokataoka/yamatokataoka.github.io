5d31d8cffc58a88d44a82ae93573a6d823991906
Yamato Kataoka <34284486+yamatokataoka@users.noreply.github.com>
2022-05-30 05:45:29 +0000

    Update my intro

--- components/about.tsx
+++ components/about.tsx
@@ -3,28 +3,8 @@ export default function About() {
     <>
       <h3>About</h3>
       <p>
-        I&apos;m a software engineer at Yahoo Japan. I recently joined the
-        company as a backend developer for an advertisement system with Golang.
-        Currently, I&apos;m insterested in the intersection of software
-        engineering, deep reinforcement learning and Human Computer Interaction.
-      </p>
-      <p>
-        I became interested in AI safety through 80,000 Hours when I was working
-        a software engineer and thinking about my future career. And I continued
-        to read some AI value alignment papers. Now my current research interest
-        is aligning AI systems with human preferences to enable AI to work with,
-        around, and in support of people. For sophisticated RL systems to
-        interact usefully with real-world environments, it is important to
-        explore goals defined in terms of human preferences to communicate
-        complex goals to these systems.
-      </p>
-      <p>
-        From the time I met AI alignment, the idea of helping others has changed
-        as a software engineer. I was always thinking better interfaces and
-        backend systems is only way to help to improve their life but now I
-        realised that there is another strong system I can contribute to, AI
-        systems. I would like to align AI systems with human preferences to
-        empower individuals to thrive in their life.
+        I&apos;m a software engineer interested in developing software systems
+        to work with, around, and in support of people.
       </p>
     </>
   );
