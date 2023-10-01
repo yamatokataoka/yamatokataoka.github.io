921cefbef64848b41352734413927cbe1df344fb
Yamato Kataoka <34284486+yamatokataoka@users.noreply.github.com>
2023-10-01 10:33:41 +0000

    Upgrade next.js from 11 to 13

--- components/navbar/index.tsx
+++ components/navbar/index.tsx
@@ -5,22 +5,18 @@ import styles from "./navbar.module.scss";
 export default function Navbar({ name, description, avatarSrc }) {
   return (
     <header className={styles.container}>
-      <Link href="/" passHref>
-        <a href="replace by passHref" className={styles.profile}>
+      <Link href="/" className={styles.profile}>
+        <>
           <Avatar src={avatarSrc} alt={name} />
           <div className={styles.profileContent}>
-            <Link href="/" passHref>
-              <a href="replace by passHref" className={styles.name}>
-                {name}
-              </a>
+            <Link href="/" className={styles.name}>
+              {name}
             </Link>
-            <Link href="/" passHref>
-              <a href="replace by passHref" className={styles.description}>
-                {description}
-              </a>
+            <Link href="/" className={styles.description}>
+              {description}
             </Link>
           </div>
-        </a>
+        </>
       </Link>
     </header>
   );
