0ad2eb5b821b95727b30282e30c4a63c90a4c059
Yamato Kataoka <34284486+yamatokataoka@users.noreply.github.com>
2023-10-05 23:14:31 +0000

    Migrate pages

---
+++ app/page.tsx
@@ -0,0 +1,11 @@
+import React from "react";
+import About from "../components/about";
+import Container from "../components/container";
+
+export default function Index() {
+  return (
+    <Container>
+      <About />
+    </Container>
+  );
+}
