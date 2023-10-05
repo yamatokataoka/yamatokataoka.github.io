017acec1f74566d882a0d81ff32ebda670f9d696
Yamato Kataoka <34284486+yamatokataoka@users.noreply.github.com>
2023-10-05 13:40:32 +0000

    Create root layout

---
+++ app/layout.tsx
@@ -0,0 +1,32 @@
+import { Metadata } from "next";
+import Head from "next/head";
+import Layout from "../components/layout";
+import "../styles.scss";
+import config from "../config.json";
+
+export const metadata: Metadata = {
+  title: `${config.name} - ${config.description}`,
+};
+
+export default function RootLayout({ children }) {
+  return (
+    <html lang="en">
+      <Head>
+        <link
+          href="https://fonts.googleapis.com/css2?family=Roboto&display=swap"
+          rel="stylesheet"
+        />
+      </Head>
+      <body>
+        <Layout
+          name={config.name}
+          description={config.description}
+          avatarSrc={config.avatarSrc}
+          githubLink={config.githubLink}
+        >
+          {children}
+        </Layout>
+      </body>
+    </html>
+  );
+}
