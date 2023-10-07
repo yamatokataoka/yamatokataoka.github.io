c96b428d654e3b7b99430656ffdf7b788a6e411b
Yamato Kataoka <34284486+yamatokataoka@users.noreply.github.com>
2023-10-07 10:34:35 +0000

    Rename module css

--- components/navbar/index.tsx
+++ components/navbar/index.tsx
@@ -1,6 +1,6 @@
 import Link from "next/link";
 import Avatar from "../avatar";
-import styles from "./navbar.module.scss";
+import styles from "./styles.module.scss";
 
 export default function Navbar({ name, description, avatarSrc }) {
   return (
