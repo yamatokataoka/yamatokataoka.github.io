1d577335fbf26958485fdb7049c5ea158961c4db
Yamato Kataoka <34284486+yamatokataoka@users.noreply.github.com>
2023-10-07 11:49:29 +0000

    Simplify image path

--- components/avatar/index.tsx
+++ components/avatar/index.tsx
@@ -1,11 +1,10 @@
 import Image from "next/image";
-import avatarPic from "../../public/avatar.png";
 import styles from "./styles.module.scss";
 
 export default function Avatar({ alt }) {
   return (
     <div className={styles.avatar}>
-      <Image src={avatarPic} alt={alt} width="40" height="40" />
+      <Image src="/avatar.png" alt={alt} width="40" height="40" />
     </div>
   );
 }
